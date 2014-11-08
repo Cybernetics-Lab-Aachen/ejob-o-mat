b5b043e40ab7094678eaec40511fd8ab3e6da6ca
Thorsten Sommer <service@sommer-engineering.com>
2014-11-08 13:06:25 +0100

    Main
    + Added the main

---
+++ Main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/SommerEngineering/Ocean/Log"
+	LM "github.com/SommerEngineering/Ocean/Log/Meta"
+	"github.com/SommerEngineering/Ocean/System"
+)
+
+func main() {
+	Log.LogShort(senderName, LM.CategoryAPP, LM.LevelINFO, LM.MessageNameSTARTUP, `Re4EEE is starting.`)
+	System.InitHandlers()
+	System.StartAndBlockForever()
+}
