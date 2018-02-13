17dea84461e277d4fe01df138a0d3210e1ca1c4e
unknown <robin.kupper@rwth-aachen.de>
2018-02-13 11:36:45 +0100

    Validate given answers.

--- Website/HandlerAnswers.go
+++ Website/HandlerAnswers.go
@@ -13,47 +13,48 @@ import (
 
 //HandlerAnswer stores the given answer and redirects to the next question or results page.
 func HandlerAnswer(response http.ResponseWriter, request *http.Request) {
-	noText := request.FormValue(`no`)
 	session := request.FormValue(`session`)
 	data := request.FormValue(`a`)
-	important := request.FormValue(`important`)
 	lang := request.FormValue(`lang`)
 	survey, loadSurveyError := DB.LoadAnswers(session)
-	no := 0
-	weight := 1
 
-	//Validate input
-	if len(lang) > 6 || len(noText) > 2 || len(data) > 16 {
-		response.WriteHeader(http.StatusNotFound)
+	// Check if session does not exists or is outdated
+	if loadSurveyError || survey.SchemeVersion < Scheme.CurrentVersion {
+		http.Redirect(response, request, fmt.Sprintf("/start?lang=%s", lang), 302)
 		return
 	}
 
-	// Check if session exists, otherwise redirect to start page
-	if loadSurveyError {
-		http.Redirect(response, request, `/start`, 302)
-		return
-	}
-
-	//Check for old session. Can't work with outdated data.
-	if survey.SchemeVersion < Scheme.CurrentVersion {
-		http.Redirect(response, request, `/start`, 302)
+	// Read question number
+	no, errNo := strconv.Atoi(request.FormValue(`no`))
+	if errNo != nil || no < 0 || no >= len(XML.Questions) { // Invalid question number
+		http.Redirect(response, request, fmt.Sprintf("/start?lang=%s", lang), 302)
 		return
 	}
 
 	//Check if question is marked as important
-	if important == `important` {
+	weight := 1
+	if request.FormValue(`important`) == `important` {
 		weight = 2
 	}
 
-	if value, errValue := strconv.Atoi(noText); errValue != nil {
-		no = 0
-	} else {
-		no = value
-
-		// Store the new answer:
-		survey.Answers[survey.Questions[no]] = Scheme.Answer{TimeUTC: time.Now().UTC(), Data: data, Weight: byte(weight)}
-		DB.UpdateAnswers(survey)
+	//Validate input data
+	dataValid := false
+	for _, button := range XML.Questions[survey.Questions[no]].Buttons { // Given data should match one of the response options
+		if data == button.Data {
+			dataValid = true
+		}
 	}
+	if !dataValid { // Data was not valid, take question again
+		http.Redirect(response, request, fmt.Sprintf("/question%d?lang=%s&session=%s", no, lang, session), 302)
+		return
+	}
+
+	// Store the new answer:
+	survey.Answers[survey.Questions[no]] = Scheme.Answer{
+		TimeUTC: time.Now().UTC(),
+		Data:    data,
+		Weight:  byte(weight)}
+	DB.UpdateAnswers(survey)
 
 	if no+1 >= len(XML.Questions) {
 		// Last questions already answered
